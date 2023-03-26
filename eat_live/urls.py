22666ae105c2a4a44c9d6f897a8f90eafc39e722
Aleksandra Sobiesiak <aleksandrasobiesiak.2@gmail.com>
2023-03-26 21:34:12 +0200

    home and calculators subpages with basic css

--- eat_live/urls.py
+++ eat_live/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from frontend.views import home
+from frontend.views import home, calculators
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('calculators/', calculators)
 ]
