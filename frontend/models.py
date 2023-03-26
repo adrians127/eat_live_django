9db15ead4ea4976d1b7dd1e10802a854600e8cc4
Aleksandra Sobiesiak <aleksandrasobiesiak.2@gmail.com>
2023-03-26 23:28:53 +0200

    calculators form works

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class CalculationForm(models.Model):
+    age = models.IntegerField()
+    weight = models.IntegerField()
+    height = models.IntegerField()
+    gender = models.CharField(max_length=20)
