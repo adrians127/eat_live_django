7815939506351de24b16fcdc7edfb1274cf46104
Aleksandra Sobiesiak <aleksandrasobiesiak.2@gmail.com>
2023-05-03 21:37:22 +0200

    display target nutritions, correction in template

--- tracker/views.py
+++ tracker/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from db_app.models import MOMENT_OF_DAY_CHOICES, MealLog
 from .forms import AddMealLogForm
+from calculators.calories_calculator import calculate_nutritions
+from members.models import Profile
+
+@login_required
+def calculate_daily_stats(request):
+    # meal_logs = MealLog.objects.filter(user=request.user.profile, date=timezone.now().date())
+    person = request.user.profile
+    person_data = calculate_nutritions(person.weight, person.height, person.age, person.gender, person.activity_level)
+    # person_data = calculate_calories(60, 170, 20, 'M', 1.2)
+    return person_data
 
 # Create your views here.
 def home(request):
     if request.user.is_authenticated:
         meal_logs = MealLog.objects.filter(user=request.user.profile, date=timezone.now().date())
+        data = calculate_daily_stats(request)
+        # data = (20, 20)
         context = {'moment_of_day_choices': MOMENT_OF_DAY_CHOICES,
-                   'meal_logs': meal_logs}
+                   'meal_logs': meal_logs,
+                   'data': data}
         
         return render(request, 'home.html', context)
     else:
