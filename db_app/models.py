7a50ee0d92bcc553b765defec144cd29c417ef97
Aleksandra Sobiesiak <aleksandrasobiesiak.2@gmail.com>
2023-05-22 15:24:47 +0200

    add description to recipe model

--- db_app/models.py
+++ db_app/models.py
@@ -33,6 +33,7 @@ class Product(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, default="No description")
 
     def __str__(self):
         return self.name
